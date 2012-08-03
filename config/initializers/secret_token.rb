250c52f7aab0b88150209bb27395971aad3a0160
Garima Singh <igarimasingh@gmail.com>
2012-08-03 14:14:53 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningAutomation::Application.config.secret_token = '9431edddc5b0f3a82f4277ea0f540519d7f212f6eeadb4e81814fb2d67fb3bd277e5e345822a92518e5c2f0d1c3f6a9438a343a4649a9c152e626b28be02c858'
